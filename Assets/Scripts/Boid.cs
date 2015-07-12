9240901d14124aef5d88ba80f28916f94eb30ffb
Simon Chauvin <contact@simonchauvin.com>
2015-07-12 18:45:26 +0200

    Boid and flock scripts, prefabs, meshes and materials.

---
+++ Assets/Scripts/Boid.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boid : MonoBehaviour
+{
+    #region Parameters
+    public float neighborRadius;
+    public float desiredSeparation;
+    public bool debug;
+    #endregion
+
+    #region Components
+    public Rigidbody thisRigidbody { get; private set; }
+    private Transform neighborArea;
+    private Transform separationArea;
+    #endregion
+
+    private FlockManager flockManager;
+
+	
+	void Start ()
+    {
+        // Components
+        thisRigidbody = GetComponent<Rigidbody>();
+        neighborArea = transform.FindChild("NeighborArea");
+        separationArea = transform.FindChild("SeparationArea");
+
+        // Flock
+        flockManager = GetComponentInParent<FlockManager>();
+
+        // Init
+        transform.position = new Vector3(Random.value * 10f, Random.value * 10f, Random.value * 10f);
+        //transform.position = new Vector3(Random.value * 10f, 0f, Random.value * 10f);
+        thisRigidbody.velocity = new Vector3(Random.value * 2 - 1, Random.value * 2 - 1, Random.value * 2 - 1);
+        //thisRigidbody.velocity = new Vector3(Random.value * 2 - 1, 0f, Random.value * 2 - 1);
+
+        neighborArea.localScale = new Vector3(neighborRadius * 2, neighborRadius * 2, neighborRadius * 2);
+        separationArea.localScale = new Vector3(desiredSeparation * 2, desiredSeparation * 2, desiredSeparation * 2);
+        neighborArea.gameObject.SetActive(false);
+        separationArea.gameObject.SetActive(false);
+	}
+	
+	
+	void Update ()
+    {
+        transform.rotation = Quaternion.LookRotation(thisRigidbody.velocity);
+
+        if (debug)
+        {
+            neighborArea.gameObject.SetActive(true);
+            separationArea.gameObject.SetActive(true);
+        }
+        else
+        {
+            neighborArea.gameObject.SetActive(false);
+            separationArea.gameObject.SetActive(false);
+        }
+	}
+
+    void FixedUpdate()
+    {
+        /*foreach (Collider collider in Physics.OverlapSphere(transform.localPosition, distance, LayerMask.GetMask("Boid")))
+        {
+
+        }*/
+
+        boundPosition();
+        //warpPosition();
+    }
+
+    private void boundPosition ()
+    {
+        if (Vector3.Distance(transform.localPosition, Vector3.zero) > 40f)
+        {
+            //thisRigidbody.AddForce((Vector3.zero - transform.localPosition) * 0.01f);
+            thisRigidbody.velocity += (Vector3.zero - transform.localPosition) * 0.1f * Time.deltaTime;
+        }
+    }
+
+    private void warpPosition ()
+    {
+        if (Vector3.Distance(transform.localPosition, Vector3.zero) > 40f)
+        {
+            transform.localPosition = -transform.localPosition + (transform.localPosition - Vector3.zero).normalized;
+        }
+    }
+
+    public void showAlignmentDebug(Vector3 velocity)
+    {
+        Debug.DrawRay(transform.localPosition, velocity, Color.blue);
+    }
+
+    public void showCohesionDebug(Vector3 velocity)
+    {
+        Debug.DrawRay(transform.localPosition, velocity, Color.green);
+    }
+
+    public void showSeparationDebug(Vector3 velocity)
+    {
+        Debug.DrawRay(transform.localPosition, velocity, Color.red);
+    }
+}
---
+++ Assets/Scripts/FlockManager.cs
@@ -0,0 +1,131 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlockManager : MonoBehaviour
+{
+
+    public Boid boidPrefab;
+    public int numberOfBoids;
+    public float alignmentWeight;
+    public float cohesionWeight;
+    public float separationWeight;
+
+
+    private Boid[] boids;
+
+	
+	void Start ()
+    {
+        boids = new Boid[numberOfBoids];
+
+        for (int i = 0; i < numberOfBoids; i++)
+        {
+            boids[i] = Instantiate(boidPrefab, transform.position, Quaternion.identity) as Boid;
+            boids[i].transform.parent = transform;
+        }
+	}
+	
+	
+	void FixedUpdate ()
+    {
+        for (int i = 0; i < numberOfBoids; i++)
+        {
+            Boid boid = boids[i];
+            if (boid != null && boid.thisRigidbody != null)
+            {
+                Vector3 alignment = align(boid) * alignmentWeight * Time.deltaTime;
+                Vector3 cohesion = cohere(boid) * cohesionWeight * Time.deltaTime;
+                Vector3 separation = separate(boid) * separationWeight * Time.deltaTime;
+                if (boid.debug)
+                {
+                    boid.showAlignmentDebug(alignment);
+                    boid.showCohesionDebug(cohesion);
+                    boid.showSeparationDebug(separation);
+                }
+
+                boid.thisRigidbody.velocity += (alignment + cohesion + separation);
+                //boid.thisRigidbody.AddForce(align(boid) * alignmentWeight);
+                //boid.thisRigidbody.AddForce(cohere(boid) * cohesionWeight);
+                //boid.thisRigidbody.AddForce(separate(boid) * separationWeight);
+            }
+        }
+	}
+
+    private Vector3 align(Boid boid)
+    {
+        Vector3 velocity = Vector3.zero;
+        int count = 0;
+        for (int i = 0; i < numberOfBoids; i++)
+        {
+            if (!boids[i].Equals(boid))
+            {
+                float distance = Vector3.Distance(boids[i].transform.localPosition, boid.transform.localPosition);
+                if (distance > 0 && distance < boid.neighborRadius)
+                {
+                    velocity += boids[i].thisRigidbody.velocity;
+                    count++;
+                }
+            }
+        }
+        if (count > 0)
+        {
+            return (velocity / (numberOfBoids - 1)).normalized;
+        }
+        else
+        {
+            return Vector3.zero;
+        }
+    }
+
+    private Vector3 cohere (Boid boid)
+    {
+        Vector3 centerOfMass = Vector3.zero;
+        int count = 0;
+        for (int i = 0; i < numberOfBoids; i++)
+        {
+            if (!boids[i].Equals(boid))
+            {
+                float distance = Vector3.Distance(boids[i].transform.localPosition, boid.transform.localPosition);
+                if (distance > 0 && distance < boid.neighborRadius)
+                {
+                    centerOfMass += boids[i].transform.localPosition;
+                    count++;
+                }
+            }
+        }
+        if (count > 0)
+        {
+            return ((centerOfMass / (numberOfBoids - 1)) - boid.transform.localPosition).normalized;
+        }
+        else
+        {
+            return Vector3.zero;
+        }
+    }
+
+    private Vector3 separate (Boid boid)
+    {
+        Vector3 velocity = Vector3.zero;
+        int count = 0;
+        for (int i = 0; i < numberOfBoids; i++)
+        {
+            if (!boids[i].Equals(boid))
+            {
+                float distance = Vector3.Distance(boids[i].transform.localPosition, boid.transform.localPosition);
+                if (distance > 0 && distance < boid.desiredSeparation)
+                {
+                    velocity -= (boids[i].transform.localPosition - boid.transform.localPosition).normalized / distance;
+                    count++;
+                }
+            }
+        }
+        if (count > 0)
+        {
+            return (velocity / (numberOfBoids - 1)).normalized;
+        }
+        else
+        {
+            return Vector3.zero;
+        }
+    }
+}
